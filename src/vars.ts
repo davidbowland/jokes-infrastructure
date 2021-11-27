ccf4ec26732b8b6dc07e97dbda503db7d3801c32
David Bowland <dbowland1@gmail.com>
2021-11-27 17:37:11 -0600

    Switch to jokes.bowland.link domain

--- src/vars.ts
+++ src/vars.ts
@@ -1,5 +1,5 @@
 export const createdBy = 'dbowland-pulumi-jokes'
 export const createdFor = 'dbowland-jokes'
 
-export const defaultOrigin = 'https://dbowland.com'
+export const defaultOrigin = 'https://jokes.bowland.link'
 export const cloudFrontOrigin = 'https://d8m2rj7f9egv3.cloudfront.net/'

978489465facbabdb5c9adb867d27249111a9279
David Bowland <dbowland1@gmail.com>
2021-11-27 22:54:15 -0600

    Fix token validity time

--- src/cognito/user-pool-clients.ts
+++ src/cognito/user-pool-clients.ts
@@ -6,8 +6,8 @@ import { cloudFrontOrigin, defaultOrigin, projectName } from '@vars'
 // https://www.pulumi.com/docs/reference/pkg/aws/cognito/userpoolclient/
 
 const tokenValidity = {
-  accessTokenValidity: 30, // Minutes
-  idTokenValidity: 30, // Minutes
+  accessTokenValidity: 0.5, // Hours
+  idTokenValidity: 0.5, // Hours
   refreshTokenValidity: 30, // Days
 }
 

9111881f5d31ea68643fa0834af1d2c97384d4be
David Bowland <dbowland1@gmail.com>
2021-11-27 21:19:01 -0600

    Generalize projectName

--- src/cognito/user-pool-domains.ts
+++ src/cognito/user-pool-domains.ts
@@ -1,10 +1,11 @@
 import * as aws from '@pulumi/aws'
 
 import { userPool } from './user-pools'
+import { projectName } from '@vars'
 
 // https://www.pulumi.com/docs/reference/pkg/aws/cognito/userpooldomain/
 
 export const userPoolDomain = new aws.cognito.UserPoolDomain('dbowland', {
-  domain: 'dbowland',
+  domain: `dbowland-${projectName}`,
   userPoolId: userPool.id,
 })
--- src/vars.ts
+++ src/vars.ts
@@ -1,5 +1,7 @@
 export const createdBy = 'dbowland-pulumi-jokes'
 export const createdFor = 'dbowland-jokes'
 
+export const projectName = 'jokes'
+
 export const defaultOrigin = 'https://jokes.bowland.link'
 export const cloudFrontOrigin = 'https://d8m2rj7f9egv3.cloudfront.net/'

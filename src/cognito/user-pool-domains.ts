5e26ec51c3c1f25085a1a60eb65c3e8031084b6e
David Bowland <dbowland1@gmail.com>
2021-12-05 22:45:55 -0600

    Update Cognito user pool domain

--- src/cognito/user-pool-domains.ts
+++ src/cognito/user-pool-domains.ts
@@ -1,11 +1,12 @@
 import * as aws from '@pulumi/aws'
 
 import { userPool } from './user-pools'
-import { projectName } from '@vars'
+import { acmCertificateArn, defaultAuthOrigin } from '@vars'
 
 // https://www.pulumi.com/docs/reference/pkg/aws/cognito/userpooldomain/
 
 export const userPoolDomain = new aws.cognito.UserPoolDomain('dbowland', {
-  domain: `dbowland-${projectName}`,
+  domain: defaultAuthOrigin,
+  certificateArn: acmCertificateArn,
   userPoolId: userPool.id,
 })

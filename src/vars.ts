a333a2b9eb9b4c4b077f026b7c1b443cd73318ac
David Bowland <dbowland1@gmail.com>
2021-12-05 23:11:49 -0600

    Update domain vars

--- src/vars.ts
+++ src/vars.ts
@@ -1,6 +1,7 @@
 /* General */
 
 export const awsAccountId = process.env.AWS_ACCOUNT_ID
+export const domainName = 'bowland.link'
 export const projectName = 'jokes'
 
 export const createdBy = `${projectName}-infrastructure`
@@ -9,6 +10,5 @@ export const createdFor = projectName
 /* Cognito */
 
 export const acmCertificateArn = `arn:aws:acm:us-east-1:${awsAccountId}:certificate/6a48cba7-feb9-4de5-8cbf-d383140fcdef`
-export const defaultAuthOrigin = `${projectName}-auth.bowland.link`
-export const defaultUIOrigin = `https://${projectName}.bowland.link`
-export const cloudFrontOrigin = 'https://d8m2rj7f9egv3.cloudfront.net/'
+export const defaultAuthOrigin = `${projectName}-auth.${domainName}`
+export const defaultUIOrigin = `https://${projectName}.${domainName}`

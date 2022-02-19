0e7467ab7745f20658de82fade4bc13714c70d03
David Bowland <dbowland1@gmail.com>
2022-02-19 12:02:06 -0600

    Add Pinpoint analytics

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,18 @@ fi
 
 # Deploy infrastructure
 
+sam deploy --stack-name jokes-infrastructure-east-1-test \
+  --template-file template-east-1.yaml --region us-east-1 \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --no-fail-on-empty-changeset \
+  --parameter-overrides Environment=test
+
+PINPOINT_ID=$(AWS_REGION=us-east-1 aws cloudformation describe-stacks --stack-name jokes-infrastructure-east-1-test --output text --query 'Stacks[0].Outputs[?OutputKey==`PinpointId`].OutputValue')
 sam deploy --stack-name jokes-infrastructure-test \
   --template-file template.yaml --region us-east-2 \
   --capabilities CAPABILITY_NAMED_IAM \
   --no-fail-on-empty-changeset \
-  --parameter-overrides AccountId=$AWS_ACCOUNT_ID Environment=test
+  --parameter-overrides Environment=test PinpointId=$PINPOINT_ID
 
 COGNITO_DOMAIN_NAME=$(aws cloudformation describe-stacks --stack-name jokes-infrastructure-test --output text --query 'Stacks[0].Outputs[?OutputKey==`UserPoolDomainName`].OutputValue')
 CLOUDFRONT_DOMAIN_NAME=$(aws cognito-idp describe-user-pool-domain --domain "$COGNITO_DOMAIN_NAME" | grep CloudFrontDistribution | cut -d \" -f4)
@@ -21,4 +28,4 @@ sam deploy --stack-name jokes-infrastructure-dns-test \
   --template-file template-dns.yaml --region us-east-2 \
   --capabilities CAPABILITY_NAMED_IAM \
   --no-fail-on-empty-changeset \
-  --parameter-overrides AccountId=$AWS_ACCOUNT_ID CloudFrontDomainName=$CLOUDFRONT_DOMAIN_NAME CognitoDomain=$COGNITO_DOMAIN_NAME Environment=test
+  --parameter-overrides CloudFrontDomainName=$CLOUDFRONT_DOMAIN_NAME CognitoDomain=$COGNITO_DOMAIN_NAME Environment=test
